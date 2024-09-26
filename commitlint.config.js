cdf9037d435f9fe946f7cd1f89584739aeed0026
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-09-26 15:49:54 +0200

    chore(test): updated commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  "scope-enum": [
+    "core",
+    "shared-errors",
+    "fastify-error-handler",
+    "fastify-logging-wrapper",
+  ],
+};
