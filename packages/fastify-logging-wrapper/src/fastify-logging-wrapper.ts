89f96309bed5637de99de5f8beeed275e74f2ef7
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-12-03 11:27:15 +0100

    chore: dependabot v5 (#17)
    * test: fixed configs
    
    * fix: fixed logging version because of published old build
    
    * chore: managed additional logs configs
    
    * feat: added single prepublish

--- packages/fastify-logging-wrapper/src/fastify-logging-wrapper.ts
+++ packages/fastify-logging-wrapper/src/fastify-logging-wrapper.ts
@@ -1,5 +1,15 @@
-import type { FastifyServerOptions, FastifyInstance } from "fastify";
+import { REQUEST_ID_HEADER } from "@ogcio/shared-errors";
+import type {
+  FastifyInstance,
+  FastifyServerOptions,
+  RawServerBase,
+} from "fastify";
+import type {
+  FastifyLoggerOptions,
+  PinoLoggerOptions,
+} from "fastify/types/logger.js";
 import hyperid from "hyperid";
+import { type DestinationStream, pino } from "pino";
 import {
   LogMessages,
   REQUEST_ID_LOG_LABEL,
@@ -12,9 +22,6 @@ import {
   resetLoggingContext,
   setLoggingContext,
 } from "./logging-wrapper.js";
-import { pino, type DestinationStream } from "pino";
-import { REQUEST_ID_HEADER } from "@ogcio/shared-errors";
-import type { PinoLoggerOptions } from "fastify/types/logger.js";
 
 const hyperidInstance = hyperid({ fixedLength: true, urlSafe: true });
 
@@ -49,11 +56,21 @@ export const initializeLoggingHooks = (server: FastifyInstance): void => {
   });
 };
 
-export const getLoggingConfiguration = (customConfig?: {
-  pinoOptions?: PinoLoggerOptions;
-  loggerDestination?: DestinationStream;
-}): FastifyServerOptions => {
-  if (customConfig)
+export const getLoggingConfiguration = (
+  customConfig?:
+    | {
+        pinoOptions?: PinoLoggerOptions;
+        loggerDestination?: DestinationStream;
+        additionalLoggerConfigs?: never;
+      }
+    | {
+        pinoOptions?: never;
+        loggerDestination?: never;
+        additionalLoggerConfigs?: FastifyLoggerOptions<RawServerBase> &
+          PinoLoggerOptions;
+      },
+): FastifyServerOptions => {
+  if (customConfig?.pinoOptions || customConfig?.loggerDestination)
     return {
       loggerInstance: pino(
         { ...getLoggerConfiguration(), ...(customConfig?.pinoOptions ?? {}) },
@@ -65,8 +82,15 @@ export const getLoggingConfiguration = (customConfig?: {
       requestIdHeader: REQUEST_ID_HEADER,
     };
 
+  let loggerConfiguration = getLoggerConfiguration();
+  if (customConfig?.additionalLoggerConfigs) {
+    loggerConfiguration = {
+      ...loggerConfiguration,
+      ...customConfig?.additionalLoggerConfigs,
+    };
+  }
   return {
-    logger: getLoggerConfiguration(),
+    logger: loggerConfiguration,
     disableRequestLogging: true,
     genReqId: () => hyperidInstance(),
     requestIdLogLabel: REQUEST_ID_LOG_LABEL,
