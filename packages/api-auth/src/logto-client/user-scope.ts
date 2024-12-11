831073b1fa7a04f51db5b2950bfd0968b0b683f4
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-12-11 09:13:58 +0100

    Chore(v4) use const instead of enum (#21)
    * chore: added test
    
    * chore(api-auth): updated version

--- packages/api-auth/src/logto-client/user-scope.ts
+++ packages/api-auth/src/logto-client/user-scope.ts
@@ -1,10 +1,10 @@
-export declare enum UserScope {
-  Profile = "profile",
-  Email = "email",
-  Phone = "phone",
-  CustomData = "custom_data",
-  Identities = "identities",
-  Roles = "roles",
-  Organizations = "urn:logto:scope:organizations",
-  OrganizationRoles = "urn:logto:scope:organization_roles",
-}
+export const UserScope = {
+  Profile: "profile",
+  Email: "email",
+  Phone: "phone",
+  CustomData: "custom_data",
+  Identities: "identities",
+  Roles: "roles",
+  Organizations: "urn:logto:scope:organizations",
+  OrganizationRoles: "urn:logto:scope:organization_roles",
+};
