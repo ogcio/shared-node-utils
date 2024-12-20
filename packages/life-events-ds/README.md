5217a1ec31d7a8b42b3293d3ed9f46c2094d3fbd
Matteo La Cognata <matteo.lacognata@gmail.com>
2024-12-20 14:22:20 +0100

    chore(): add life-events-ds package (#33)
    * chore(): add package
    
    * chore(): cleanup
    
    * chore(): fix lock
    
    * chore(): yes
    
    * chore(): rename package
    
    * chore(): add lock
    
    * chore(): apply suggestions

---
+++ packages/life-events-ds/README.md
@@ -0,0 +1,19 @@
+# Next Design System
+
+A wrapping package for the [OGCIO design system](https://storybook.design-system.ogcio.gov.ie/?path=/story/docs-get-started--page), using only the sass from the original repo.
+
+Goal is to provide the css and progressive enhancement ready react components.
+
+## Usage
+
+All sass are bundled into a css file which you import (at the moment) at some global level by
+`import "@ogcio/life-events-ds/style.css";`
+
+Icons are available as a react component called `Icon`. The css has to be imported separately by
+`import "@ogcio/life-events-ds/esm/index.css"`
+
+Use class names from storybook unless a react component exists.
+
+## Note
+
+This is very much work in progress and is certainly subject to rapid and breaking changes.
---
+++ packages/life-events-ds/src/Icon.tsx
@@ -0,0 +1,54 @@
+// biome-ignore lint/style/useImportType: <explanation>
+import React from "react";
+import "./icon.css";
+
+type Props = {
+  className?: string;
+  color?: string;
+  size?: number;
+  width?: number;
+  heigth?: number;
+  icon:
+  | "events"
+  | "about"
+  | "birth"
+  | "health"
+  | "driving"
+  | "employment"
+  | "business"
+  | "housing"
+  | "death"
+  | "payments"
+  | "providers"
+  | "login"
+  | "logout"
+  | "notification"
+  | "search"
+  | "hamburger-menu"
+  | "tiles"
+  | "messaging-service"
+  | "payments-service"
+  | "send-a-message"
+  | "template"
+  | "settings"
+  | "check-mark"
+  | "journey-builder"
+  | "notification-settings";
+};
+
+export const Icon = ({ className, color, icon, size, heigth, width }: Props) => {
+  const iconClassName = `ds-icon-${icon}`;
+  const _className = ["ds-icon", iconClassName, className || ""].join(" ");
+  const style: React.CSSProperties = { color };
+  if (size) {
+    style.height = `${size}px`;
+    style.width = `${size}px`;
+  }
+
+  if (heigth && width) {
+    style.height = `${heigth}px`;
+    style.width = `${width}px`;
+  }
+
+  return <span className={_className} style={style} />;
+};
