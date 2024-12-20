f820c050eb64f4ad9d8e5eb73f2d34c4b8db072b
Matteo La Cognata <matteo.lacognata@gmail.com>
2024-12-20 11:33:17 +0100

    chore(): improve a bit

--- packages/nextjs-analytics/src/types.ts
+++ packages/nextjs-analytics/src/types.ts
@@ -2,14 +2,17 @@ import type {
   BuildingBlocksSDK,
   getBuildingBlockSDK,
 } from "@ogcio/building-blocks-sdk";
-import type { GetOrganizationTokenParams } from "@ogcio/building-blocks-sdk/dist/types";
+import type {
+  GetAccessTokenParams,
+  GetOrganizationTokenParams,
+} from "@ogcio/building-blocks-sdk/dist/types";
 
 type AnalyticsClientProps = ReturnType<typeof getBuildingBlockSDK>["analytics"];
 
 type AnalyticsConfigProps = Parameters<
   typeof getBuildingBlockSDK
 >["0"]["services"]["analytics"] &
-  GetOrganizationTokenParams;
+  (GetAccessTokenParams | GetOrganizationTokenParams);
 
 type AnalyticsTrackerProps = Parameters<
   AnalyticsClientProps["setTrackingContext"]
