54107da6fe3b53bcdc5a9fa061998e64d1503c1b
bozzelliandrea <bozzelliandrea@yahoo.it>
2025-03-13 18:57:25 +0100

    feat: add access control headers to o11y plugin

--- packages/fastify-o11y-plugin/__test__/build-fastify.ts
+++ packages/fastify-o11y-plugin/__test__/build-fastify.ts
@@ -1,12 +1,11 @@
 import Fastify from "fastify";
-import o11yPlugin, { X_TRACE_ID } from "../src/index";
-import { getActiveSpan } from "@ogcio/o11y-sdk-node";
+import o11yPlugin from "../src/index";
 
 export const buildFastify = () => {
   const server = Fastify();
   server.register(o11yPlugin);
 
-  server.get("/", function (_request, reply) {
+  server.get("/", (_request, reply) => {
     reply.send({ hello: "world" });
   });
 
--- packages/fastify-o11y-plugin/src/index.ts
+++ packages/fastify-o11y-plugin/src/index.ts
@@ -2,7 +2,7 @@ import { getActiveSpan, getMetric } from "@ogcio/o11y-sdk-node";
 import type { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 
-export const X_TRACE_ID = "x-trace-id";
+const ACCESS_CONTROL_EXPOSE_HEADERS = "access-control-request-headers";
 
 const httpResponsesCounter = getMetric<"counter", { status_code: number }>(
   "counter",
@@ -12,23 +12,40 @@ const httpResponsesCounter = getMetric<"counter", { status_code: number }>(
   },
 );
 
-export default fp(async (server: FastifyInstance) => {
-  server.addHook("onRequest", (_request, reply, done) => {
-    try {
-      const traceId = getActiveSpan()?.spanContext()?.traceId;
-      if (traceId) {
-        reply.header(X_TRACE_ID, traceId);
-      }
-    } finally {
-      done();
-    }
-  });
+export const X_TRACE_ID = "x-trace-id";
 
-  server.addHook("onResponse", (_request, reply, done) => {
-    try {
-      httpResponsesCounter.add(1, { status_code: reply.statusCode });
-    } finally {
-      done();
-    }
-  });
-});
+export default fp(
+  async (server: FastifyInstance) => {
+    server.addHook("onRequest", (request, reply, done) => {
+      try {
+        const traceId = getActiveSpan()?.spanContext()?.traceId;
+        if (traceId) {
+          if (request.headers[ACCESS_CONTROL_EXPOSE_HEADERS]) {
+            reply.header(
+              ACCESS_CONTROL_EXPOSE_HEADERS,
+              `${X_TRACE_ID}, ${request.headers[ACCESS_CONTROL_EXPOSE_HEADERS]}`,
+            );
+          } else {
+            reply.header(ACCESS_CONTROL_EXPOSE_HEADERS, X_TRACE_ID);
+          }
+
+          reply.header(X_TRACE_ID, traceId);
+        }
+      } finally {
+        done();
+      }
+    });
+
+    server.addHook("onResponse", (_request, reply, done) => {
+      try {
+        httpResponsesCounter.add(1, { status_code: reply.statusCode });
+      } finally {
+        done();
+      }
+    });
+  },
+  {
+    fastify: "5.x",
+    name: "@ogcio/fastify-o11y-plugin",
+  },
+);
