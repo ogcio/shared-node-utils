a9d8c0470d57eadaf1ef874e0d63676136fbd0c2
Samuele Salvatico <samuele.salvatico@nearform.com>
2025-03-14 16:18:10 +0100

    chore: fixed const exports

--- packages/nextjs-auth/src/utils/constants.ts
+++ packages/nextjs-auth/src/utils/constants.ts
@@ -3,3 +3,5 @@ export const INACTIVE_PUBLIC_SERVANT_ORG_ROLE =
   "inactive-ps-org:Inactive Public Servant";
 
 export const SELECTED_ORG_COOKIE = "bb-selected-org-id";
+
+export const DEFAULT_LOGIN_PATH = "/login";
