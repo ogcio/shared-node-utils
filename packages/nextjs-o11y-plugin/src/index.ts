6dabbb7c72a3fdf26d9e6f3f05a998d3f310b87a
bozzelliandrea <bozzelliandrea@yahoo.it>
2025-03-19 11:47:47 +0100

    feat: o11y next middleware

---
+++ packages/nextjs-o11y-plugin/src/index.ts
@@ -0,0 +1,34 @@
+import { trace } from "@opentelemetry/api";
+
+export const ACCESS_CONTROL_EXPOSE_HEADERS = "access-control-expose-headers";
+export const X_TRACE_ID = "x-trace-id";
+
+export default function o11y<
+  HttpRequest extends Request,
+  HttpResponse extends Response,
+>(_req: HttpRequest, res: HttpResponse): void {
+  try {
+    const traceId = trace.getActiveSpan()?.spanContext()?.traceId;
+
+    if (traceId) {
+      res.headers.set(X_TRACE_ID, traceId);
+    }
+
+    const exposedHeaders = res.headers.get(ACCESS_CONTROL_EXPOSE_HEADERS);
+
+    if (exposedHeaders) {
+      if (exposedHeaders.toString().includes(X_TRACE_ID)) {
+        return;
+      }
+
+      res.headers.set(
+        ACCESS_CONTROL_EXPOSE_HEADERS,
+        `${X_TRACE_ID}, ${exposedHeaders}`,
+      );
+    } else {
+      res.headers.set(ACCESS_CONTROL_EXPOSE_HEADERS, X_TRACE_ID);
+    }
+  } catch (error) {
+    console.error("Observability Middleware Error:", error);
+  }
+}
